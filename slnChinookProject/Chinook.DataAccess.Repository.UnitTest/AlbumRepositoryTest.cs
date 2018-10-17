0de29ba82f7a9b73592779935287138f677929e6
julial2904 <jsegil.rodriguez@gmail.com>
2018-10-16 22:16:12 -0500

    Laboratorio 07 inicio

---
+++ slnChinookProject/Chinook.DataAccess.Repository.UnitTest/AlbumRepositoryTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Data.Entity;
+using Chinook.DataAccess.Repository.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Chinook.Entities.Base;
+
+namespace Chinook.DataAccess.Repository.UnitTest
+{
+    [TestClass]
+    public class AlbumRepositoryTest
+    {
+        private readonly DbContext dbContext;
+        private readonly IAlbumRepository albumRepository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public AlbumRepositoryTest()
+        {
+            dbContext = new ChinookDBModel();
+            albumRepository = new AlbumRepository(dbContext);
+            unitOfWork = new UnitOfWork(dbContext);
+        }
+
+        [TestMethod]
+        public void GetCountAlbum()
+        {
+            var cantidad = albumRepository.Count();
+            Assert.IsTrue(cantidad > 0, "OK");
+        }
+
+
+        [TestMethod]
+        public void GetAllAlbum()
+        {
+            var album = albumRepository.GetAll();
+            Assert.IsTrue(album.Count() > 0, "OK");
+        }
+
+        [TestMethod]
+        public void GetByidAlbum()
+        {
+            int idPrueba = 52;
+            var album = albumRepository.GetByid(idPrueba);
+            Assert.IsTrue(album.Title != "", "OK");
+        }
+
+        [TestMethod]
+        public void AddAlbum()
+        {
+            var album = new Album();
+            album.Title = "Test Album";
+            album.ArtistId = 70;
+            albumRepository.Add(album);
+            dbContext.SaveChanges();
+
+            Assert.IsTrue(album.AlbumId > 0, "OK");
+
+        }
+
+        [TestMethod]
+        public void RemoveAlbum()
+        {
+            var album = new Album();
+            album.AlbumId = 350;
+            albumRepository.Remove(album);
+            var registroseliminados = dbContext.SaveChanges();
+
+            Assert.IsTrue(registroseliminados > 0, "OK");
+
+        }
+
+        [TestMethod]
+        public void AlbunesVendidos()
+        {
+            var listado = unitOfWork.AlbumRepository.GetAlbunesVendidos();
+            unitOfWork.Dispose();
+            Assert.IsTrue(listado.Count() > 0, "OK");
+        }
+
+    }
+}
---
+++ slnChinookProject/Chinook.DataAccess.Repository.UnitTest/InvoiceRepositoryTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Entity;
+using Chinook.DataAccess.Repository.Interfaces;
+using Chinook.Entities.Base;
+using System.Linq;
+
+namespace Chinook.DataAccess.Repository.UnitTest
+{
+    /// <summary>
+    /// Descripción resumida de InvoiceRepositoryTest
+    /// </summary>
+    [TestClass]
+    public class InvoiceRepositoryTest
+    {
+        private readonly DbContext dbContext;
+        private readonly IInvoiceRepository invoiceRepository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public InvoiceRepositoryTest()
+        {
+            dbContext = new ChinookDBModel();
+            unitOfWork = new UnitOfWork(dbContext);
+        }
+
+        [TestMethod]
+        public void AddInvoice()
+        {
+            var invoice = new Invoice();
+            invoice.CustomerId = 2;
+            invoice.InvoiceDate = DateTime.Now;
+            invoice.BillingCity = "San Juan de Miraflores";
+            invoice.BillingAddress = "Pj. Los Lirios 1234";
+            invoice.BillingCountry = "Perú";
+            invoice.BillingPostalCode = "Lima 29";
+
+            invoice.InvoiceLine = new List<InvoiceLine>();
+            //Item1
+            invoice.InvoiceLine.Add(
+                new InvoiceLine()
+                {
+                    TrackId = 1 ,
+                    Quantity = 2,
+                    UnitPrice = 10
+                });
+            //Item2
+            invoice.InvoiceLine.Add(
+                new InvoiceLine()
+                {
+                    TrackId = 2,
+                    Quantity = 3,
+                    UnitPrice = 10
+                });
+
+            invoice.Total = invoice.InvoiceLine
+                .Sum(item => item.UnitPrice * item.Quantity);
+
+            unitOfWork.InvoiceRepository.Add(invoice);
+            unitOfWork.Complete();
+            unitOfWork.Dispose();
+
+            Assert.IsTrue(invoice.InvoiceId > 0, "OK");
+        }
+    }
+}
